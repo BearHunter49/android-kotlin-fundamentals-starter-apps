173ec582c52b78f263bc5789db7b5822530682dc
BearHunter49 <rkdcjf987@naver.com>
2020-11-23 01:08:28 +0900

    Navigation Paths finish

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,22 +19,36 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = findNavController(R.id.myNavHostFragment)
+
+        drawerLayout = binding.drawerLayout
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
     }
 
-    // TODO (01) Create the new TitleFragment
-    // Select File->New->Fragment->Fragment (Blank)
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.myNavHostFragment).navigateUp(drawerLayout)
+    }
 
-    // TODO (02) Clean up the new TitleFragment
-    // In our new TitleFragment
 
-    // TODO (03) Use DataBindingUtil.inflate to inflate and return the titleFragment in onCreateView
-    // In our new TitleFragment
-    // R.layout.fragment_title
+
+
+
 }
